10b354d746da718ed0a907591316f3c5f66030ca
guido morabito <guidomorabito161@hotmail.com>
2023-05-03 23:05:47 -0300

    3/5 2

--- Bases Typescript/main.js
+++ Bases Typescript/main.js
@@ -1,46 +1,14 @@
 "use strict";
-var __rest = (this && this.__rest) || function (s, e) {
-    var t = {};
-    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
-        t[p] = s[p];
-    if (s != null && typeof Object.getOwnPropertySymbols === "function")
-        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
-            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
-                t[p[i]] = s[p[i]];
+(() => {
+    class Avenger {
+        constructor(name, team, realName) {
+            this.name = name;
+            this.team = team;
+            this.realName = realName;
         }
-    return t;
-};
-(() => {
-    const avengers = {
-        nick: "Samuel L",
-        ironman: "Robert",
-        vision: "Paul",
-        activo: true,
-        poder: 1500.3423
-    };
-    const printAvenger = (_a) => {
-        var { ironman } = _a, resto = __rest(_a, ["ironman"]);
-        console.log(ironman, resto);
-    };
-    const avengersArr = ["Cap", true, 144];
-    const [capi, superBoolean, numerito] = avengersArr;
-})();
-(() => {
-    const ironman = {
-        name: "Ironman",
-        weapon: "Armour"
-    };
-    const captainAmerica = {
-        name: "Capitan",
-        weapon: "Escudo"
-    };
-    const thor = {
-        name: "Thor",
-        weapon: "Mjolnir"
-    };
-    const avengers = [ironman, thor, captainAmerica];
-    for (const avenger of avengers) {
-        console.log(avenger);
     }
+    Avenger.avgAge = 35;
+    const antman = new Avenger("Antman", "Capi");
+    console.log(antman);
 })();
 //# sourceMappingURL=main.js.map
